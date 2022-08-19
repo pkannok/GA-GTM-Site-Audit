160c37ed0089c6498b50fd85a3bb7b7f7a063eca
pkannok <pkannok@me.com>
2022-08-19 13:50:42 -0600

    google ids, deduped in lists

--- scrape_network_calls.py
+++ scrape_network_calls.py
@@ -8,6 +8,8 @@ Created on Thu Aug 18 12:56:50 2022
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 import os, time, json
 
 # Set client directory for working files
@@ -30,7 +32,7 @@ if __name__ == "__main__":
     
     # Startup Chrome webdriver w/ exe path and pass options as params
     driver = webdriver.Chrome(ChromeDriverManager().install(),
-                              chrome_options=options,
+                              options=options,
                               desired_capabilities=desired_capabilities)
     
     # Send a request to the website and pause
@@ -64,10 +66,25 @@ if __name__ == "__main__":
     json_file = "network_log.json"
     with open(json_file, "r", encoding = "utf-8") as f:
         logs = json.loads(f.read())
+        gtm_lst = []
+        ua_lst = []
+        ga4_lst = []
         
         for log in logs:
             try: 
                 url = log["params"]["request"]["url"]
-                print(url, end='\n\n')
-            except Exception as e:
+                if url.find("tid=") != -1:
+                    ga_id = parse_qs(urlparse(url).query)['tid'][0]
+                    if parse_qs(urlparse(url).query)['v'][0] == "1":
+                        ua_lst.append(ga_id)
+                    if parse_qs(urlparse(url).query)['v'][0] == "2":
+                        ga4_lst.append(ga_id)
+                if url.find("gtm.js") != -1:
+                    ga_id = parse_qs(urlparse(url).query)['id'][0]
+                    gtm_lst.append(ga_id)
+            except Exception:
                 pass
+        
+        gtm_lst = [*set(gtm_lst)]
+        ua_lst = [*set(gtm_lst)]
+        ga4_lst = [*set(ga4_lst)]
